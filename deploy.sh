5a8554d3ee59c6e302323bfd9b40a4518031b8c3
tarun <tarunkumar8059@gmail.com>
2024-04-18 11:25:25 +0530

    12

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 
 ls
 pwd
+cd /opt/codedeploy-agent/deployment-root/b9a7715f-0980-407f-8127-613b0571f1b8/d-6MSXFDIH4/deployment-archive/
+cd 
 npm install &&
 npm run build &&
 pm2 start ||pm2 run start
